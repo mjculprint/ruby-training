3f36933a25928ee128980f820f6bcbc84ed2a1e5
mjculprint <62800068+mjculprint@users.noreply.github.com>
2020-05-04 17:40:30 +0200

    cannot raise errors, updated a small piece of code

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  if ((a == b) && (a == c) && (b == c))
+   if ((a == b) && (a == c) && (b == c))
      :equilateral
-  elsif ((a == b) || (a == c) || (b == c))
+   elsif ((a == b) || (a == c) || (b == c))
      :isosceles
-  else
+   elsif (a =! b =! c) 
      :scalene
-  end
+   end
 end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
-class TriangleError < StandardError
-
+class TriangleError < StandardError 
 end
