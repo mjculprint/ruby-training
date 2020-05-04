83a6780b7ee5210a7eca1ea08359aafa513ff6ec
mjculprint <62800068+mjculprint@users.noreply.github.com>
2020-05-04 14:56:14 +0200

    :isosceles is not working yet

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b || b == c
+    :equilateral
+  elsif a =! b || b == c 
+    :isosceles
+  elsif a =! c || b == c 
+    :isosceles
+  elsif c =! b || b == a 
+    :isosceles 
+  else
+    :scalene 
+  end
+
 end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
 class TriangleError < StandardError
+
 end
