99b0a95281855c0a883f05fc94cb8e8c177626f2
mjculprint <62800068+mjculprint@users.noreply.github.com>
2020-05-04 15:59:26 +0200

    once you understand, it is getting easier. quadrilateral half done

--- shapes/lib/quadrilateral.rb
+++ shapes/lib/quadrilateral.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 def quadrilateral(a, b, c, d)
-  # WRITE THIS CODE
+  if ((a == 90) && (b == 90) && (c == 90) && (d == 90))
+    [:square, :rectangle]
+  elsif (((a == b) && (c == d)) ||
+    ((a == c) && (b == d)) ||
+    ((a == d) && (b == c)) )
+    [:parallelogram, :rhombus]
+  else 
+    [:quadrilateral]
+  end
+
 end
 
 # Error class used in invalid_quadrilateral_spec. No need to change this code.
