402b7460d541393de99374fe6ca6d7e2c6ca0b37
mjculprint <62800068+mjculprint@users.noreply.github.com>
2020-05-05 12:05:44 +0200

    i did struggle with errors

--- shapes/lib/triangle.rb
+++ shapes/lib/triangle.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 def triangle(a, b, c)
-   if ((a == b) && (a == c) && (b == c))
-     :equilateral
-   elsif ((a == b) || (a == c) || (b == c))
-     :isosceles
-   elsif (a =! b =! c) 
-     :scalene
-   end
+  raise TriangleError if a<=0 or b<=0 or c<=0
+  raise TriangleError if a+b<=c or b+c<=a or a+c<=b
+  return :equilateral if a==b and a==c                 
+  return :isosceles if a==b or b==c or a==c
+  :scalene  
 end
 
 # Error class used in invalid_triangle_spec. No need to change this code.
