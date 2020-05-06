e1a93b9de5e83391a4d3613758b6621c83f77227
mjculprint <62800068+mjculprint@users.noreply.github.com>
2020-05-06 15:36:47 +0200

    it is passing

--- shapes/lib/polygon_angles.rb
+++ shapes/lib/polygon_angles.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 def internal_angle(number_of_sides)
-  return (180 * (number_of_sides - 2) )/number_of_sides 
-# I edited the the spec for 7 
+  return ((180 * (number_of_sides.to_f - 2) )/number_of_sides.to_f ).round(2)
 end
 
 def external_angle(number_of_sides)
-  return (360/number_of_sides)
-  #Failure/Error: expect(external_angle(7)).to eq(51.43)
-  # expected: 51.43
-  #  got: 51 ==== example output
+  return (360/number_of_sides.to_f).round(2)
 end
